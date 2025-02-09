66aa4442f170afca5549450c1c2182435dc38dff
unknown <zafar1219@gmail.com>
2025-02-09 12:42:54 +0530

    added readme

--- bin/string_calculator.dart
+++ bin/string_calculator.dart
@@ -1,27 +1,3 @@
-// import 'dart:io';
-// import 'package:string_calculator/string_calculator.dart';
-
-// void main() {
-//   print("Enter numbers separated by commas or newlines (press Enter after each line, and Ctrl+Z to finish):");
-//   print("To use a custom delimiter, enter in this format: `//[delimiter]\\n[numbers]` (e.g., `//;\n1;2;3`).");
-
-//   String input = '';
-//   while (true) {
-//     String? line = stdin.readLineSync();
-//     if (line == null || line.isEmpty) break;
-//     input += (input.isEmpty ? '' : '\n') + line;
-//   }
-
-//   try {
-//     int result = StringCalculator.add(input);
-//     print("Result: $result");
-//   } on FormatException catch (e) {
-//     print("Error: ${e.message}");
-//   } catch (e) {
-//     print("Error: Invalid input. Please enter valid numbers.");
-//   }
-// }
-
 
 
 import 'dart:io';
